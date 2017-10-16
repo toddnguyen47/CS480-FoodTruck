d77efc35f87c2d15e560cc0c2541641bbe28ac4a
toddnguyen47 <todd.nguyen47@gmail.com>
2017-10-15 23:48:31 -0700

    AngularJS file able to read JSON files
    Added an AngularJS file that is able to read JSON files (using JQuery)
    and print them out to the console. Next stpe is to use AngularJS to
    display the obtained data.

---
+++ src/main/resources/static/js/foodTruckDisplay-Control.js
@@ -0,0 +1,16 @@
+function loadJSON() {
+	var jsonFile = $.getJSON("output.json", function(json) {
+		var array1 = []
+		for (let elem of json) {
+			var tempDict = {};
+			tempDict["id"] = elem["id"];
+			tempDict["address"] = elem["location"];
+			tempDict["phone_number"] = elem["phone"];
+			tempDict["image_url"] = elem["image_url"];
+			tempDict["url"] = elem["url"];
+			array1.push(tempDict);
+		}
+		console.log(array1);
+		document.getElementById("displayInfo").innerHTML = array1;
+	});
+}
