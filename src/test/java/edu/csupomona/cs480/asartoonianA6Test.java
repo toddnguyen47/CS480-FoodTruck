cef844485553ab899673ae64a60248beb548a65b
Allen Artoonian <asartoonian@cpp.edu>
2017-10-24 12:27:06 -0700

    adding A6test class

---
+++ src/test/java/edu/csupomona/cs480/asartoonianA6Test.java
@@ -0,0 +1,20 @@
+package edu.csupomona.cs480;
+
+import org.junit.Test;
+import edu.csupomona.cs480.ThoNguyenA6;
+import org.junit.Assert;
+
+/**
+ * This makes sure that the file contains at least one instance of the following:
+ * name, image_url, url, coordinates, location, and display_phone.
+ * @author Allen Artoonian
+ */
+public class asartoonianA6Test {
+   @Test
+   void testGetDisplayName() {
+       asartoonianA6 person = new asartoonianA6("Josh", "Hayden");
+       String displayName = person.getDisplayName();
+       Assert.assertEquals("Hayden, Josh",person.getDisplayName());
+
+   }
+}
