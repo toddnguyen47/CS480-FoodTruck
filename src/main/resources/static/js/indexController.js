4ee4f682f2664cf4baeb86644b56e9f49136f3dc
Todd Nguyen <todd.nguyen47@gmail.com>
2017-11-04 22:21:37 -0700

    Added table functionality + added checkboxes
    To our website.

--- src/main/resources/static/js/indexController.js
+++ src/main/resources/static/js/indexController.js
@@ -1,11 +1,26 @@
 var optPrimeApp = angular.module('optPrimeApp', []);
 
-optPrimeApp.controller('inputForm', function($scope, $http){
-	$scope.myFunction = function() {
-		var tempDict = {};
-		tempDict["location"] = $scope.location;
-		
-		console.log("Button clicked!");
-		console.log(tempDict);
+optPrimeApp.controller('inputForm', ['$scope', '$http', function($scope, $http){
+	$scope.inputForm = {};
+	// Do I need to fix locationArr?
+	$scope.locationArr = [{locationType: "Zip code"}, {locationType:"Address"}, {locationType: "Current Location"}];
+
+	$scope.postSearch = function() {
+		//var tempLocation = angular.lowercase($scope.form.location1.toString());
+		//var tempSelectedMode = angular.lowercase($scope.form.selectedMode.locationType.toString());
+		//var concatString = "cs480/foodtrucks/yelp/" + tempLocation + "?opt=" + tempSelectedMode;
+		var concatString = "cs480/foodtrucks/yelp/" + $scope.form.location1 + "?opt=" + $scope.form.selectedMode.locationType;
+		$http.post(concatString).success(function(dataResponse) {
+			$scope.trucks = dataResponse;
+		});
 	}
-});
+
+	$scope.checkboxModel = {
+		American: false,
+		Italian: false,
+		Japanese: false,
+		Korean: false,
+		Mexican: false,
+		Random: false
+	};
+}]);
