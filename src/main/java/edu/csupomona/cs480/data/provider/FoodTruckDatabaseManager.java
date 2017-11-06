22af07bade1a0de5a6615059797a9e1a6074a8c8
toddnguyen47 <todd.nguyen47@gmail.com>
2017-11-06 02:16:36 -0800

    Added HTTPS file + fixed some interface issues.

---
+++ src/main/java/edu/csupomona/cs480/data/provider/FoodTruckDatabaseManager.java
@@ -0,0 +1,10 @@
+package edu.csupomona.cs480.data.provider;
+
+/**
+ * Allen, please write something in this interface!
+ * @author Allen Artoonian
+ *
+ */
+public interface FoodTruckDatabaseManager {
+	// This is empty! Allen, please write something.
+}
