1d5d716b21798e46a2541177c164229fba115b61
Allen Artoonian <asartoonian@cpp.edu>
2017-11-12 23:54:48 -0800

    adding the new SQL Schema

--- db-schema/foodtrucks.sql
+++ db-schema/foodtrucks.sql
@@ -1,29 +1,35 @@
--- MySQL dump 10.13  Distrib 5.7.20, for macos10.12 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: foodtrucks
--- ------------------------------------------------------
--- Server version	5.7.20
+-- Host: 127.0.0.1
+-- Generation Time: Nov 13, 2017 at 08:53 AM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `foodtrucks`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `truck_info`
 --
 
-DROP TABLE IF EXISTS `truck_info`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `truck_info` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` varchar(300) NOT NULL,
   `type` varchar(100) DEFAULT NULL,
   `zip_code` varchar(5) DEFAULT NULL,
@@ -33,26 +39,41 @@ CREATE TABLE `truck_info` (
   `address` varchar(300) DEFAULT NULL,
   `lat` double NOT NULL DEFAULT '0',
   `lon` double NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `image_url` varchar(255) DEFAULT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `truck_info`
 --
 
-LOCK TABLES `truck_info` WRITE;
-/*!40000 ALTER TABLE `truck_info` DISABLE KEYS */;
-/*!40000 ALTER TABLE `truck_info` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `truck_info` (`id`, `name`, `type`, `zip_code`, `phone_number`, `area_code`, `city`, `address`, `lat`, `lon`, `image_url`, `time`) VALUES
+(2, 'New Asian3', 'Thai', '91020', '2678988', '626', 'Alhambra', 'new address', 30, 30, 'Url', '2017-11-12 23:52:32'),
+(3, 't', 'Mexican', NULL, '8590903', '818', 'Beverly Hills', '1442 S Euclid Ave', 34.198064099999996, -118.23511920000001, 'URL', '2017-11-12 23:52:32'),
+(4, 'trrtg', 'Mexican', NULL, '8590903', '818', 'Beverly Hills', '1442 S Euclid Ave', 34.198064099999996, -118.23511920000001, 'URL', '2017-11-12 23:52:32'),
+(5, 'trrtg', 'Mexican', NULL, '8590903', '818', 'Beverly Hills', '1442 S Euclid Ave', 34.198064099999996, -118.23511920000001, 'URL', '2017-11-12 23:52:32');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `truck_info`
+--
+ALTER TABLE `truck_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `truck_info`
+--
+ALTER TABLE `truck_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2017-10-24 14:08:34
